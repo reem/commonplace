af93aa469aa7ed8566cb6709a5412c7cac64114b
Jonathan Reem <jonathan.reem@gmail.com>
2014-01-06 18:57:30 -0500

    Fixed lots of style and lint bugs.

--- commonplace/Items.py
+++ commonplace/Items.py
@@ -1,24 +1,25 @@
 """
 Jonathan Reem
-Implements the base classes for items such as equipment.
+Implements the classes for items such as equipment.
 """
-
-__all__ = ['BaseItem', 'BaseEquipment']
 
 from commonplace.Base import BaseObj, format_objects
 
 
 class BaseItem(BaseObj):
+
     """
     Items should inherit from this class, and should define their item_type
     in their constructor. See BaseEquipment for an example.
     """
+
     def __init__(self, name, description, item_type):
         BaseObj.__init__(self, name, description)
         self.item_type = item_type
 
 
 class BaseEquipment(BaseItem):
+
     """
     Equipment should inherit from this class.
 
@@ -26,6 +27,7 @@ class BaseEquipment(BaseItem):
     stats: a dictionary of {stat: boost} pairs.
         "boost" can be negative!
     """
+
     def __init__(self, name, description, eq_type, stats):
         BaseItem.__init__(self, name, description, 'Equipment')
         self.eq_type = eq_type
@@ -36,16 +38,20 @@ class BaseEquipment(BaseItem):
         return "Stats:\n" + format_objects(self.stats)
 
     def full_info(self):
+        "Format everything for prompting."
         return str(self)
 
     def __str__(self):
         return BaseItem.__str__(self) + '\n' + self.format_stats()
 
     def __repr__(self):
-        return BaseItem.__repr__(self) + '\n' + self.formatt_stats()
+        return BaseItem.__repr__(self) + '\n' + self.format_stats()
 
 
 class BrainEquipment(BaseEquipment):
+
+    "Equipment specific for commonplace game."
+
     def __init__(self, name, description, eq_type, stats, quote):
         BaseEquipment.__init__(self, name, description, eq_type, stats)
         self.quote = quote
@@ -56,10 +62,14 @@ class BrainEquipment(BaseEquipment):
 
 
 class BaseTreasure(BaseItem):
+
+    "Base Treasure class. All treasure inherits from this."
+
     def __init__(self, name, description):
         BaseItem.__init__(self, name, description, 'Treasure')
 
-    def fulL_info(self):
+    def full_info(self):
+        "Format everything for information/prompting."
         return str(self)
 
     def __str__(self):
@@ -67,11 +77,15 @@ class BaseTreasure(BaseItem):
 
 
 class PoemTreasure(BaseTreasure):
+
+    "Treasure for poem rooms."
+
     def __init__(self, name, description, quote, category):
         BaseTreasure.__init__(self, name, description)
         self.category = category
         self.quote = quote
 
     def full_info(self):
-        return "{}\n{}\n{}".format(BaseTreasure.__str__(self), self.quote
+        "Format everything for information/prompting."
+        return "{}\n{}\n{}".format(BaseTreasure.__str__(self), self.quote,
                                    self.category)
