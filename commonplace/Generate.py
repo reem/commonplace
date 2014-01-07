e396318b48fcc07c8914b154847cf1bfdcebad21
Jonathan Reem <jonathan.reem@gmail.com>
2014-01-07 18:43:30 -0500

    Added item generation.

--- commonplace/Generate.py
+++ commonplace/Generate.py
@@ -6,6 +6,9 @@ Generates all game objects.
 """
 
 import commonplace.Items as Items
+import commonplace.Monster as Monster
+from commonplace.Quotes import QUOTES
+from commonplace.ItemList import ITEMS
 # pylint: disable=R0903, R0913
 
 
@@ -23,19 +26,93 @@ def generate_map():
     "Generates a map."
     pass
 
+def generate_item(category, strength):
+    "Generates an item appropriate for a category and strength."
+    item_template = ITEMS[category][strength].pop()
+    quote = QUOTES[category].pop()
+    health, attack = stats_from_template(item_template)
+    stats = {'health': health, 'attack': attack}
+    return Items.BrainEquipment(item_template.name, item_template.description,
+                                item_template.item_type.lower(),
+                                quote, stats)
+
+def stats_from_template(item_template):
+    "Gets the correct stats for equipment."
+    helmet_lookup = {
+        1: (20, 0),
+        2: (50, 0),
+        3: (100, 20),
+        4: (200, 40)
+    }
+
+    armor_lookup = {
+        1: (50, 0),
+        2: (150, 0),
+        3: (450, 0),
+        4: (1400, 0)
+    }
+
+    shield_lookup = {
+        1: (30, 15),
+        2: (60, 30),
+        3: (100, 50),
+        4: (200, 120)
+    }
+
+    sword_lookup = {
+        1: (0, 20),
+        2: (0, 100),
+        3: (0, 500),
+        4: (0, 2500)
+    }
+
+    ring_lookup = {
+        1: (20, 20),
+        2: (45, 60),
+        3: (100, 150),
+        4: (200, 300)
+    }
+
+    master_lookup = {
+        'helmet': helmet_lookup,
+        'armor': armor_lookup,
+        'shield': shield_lookup,
+        'sword': sword_lookup,
+        'ring': ring_lookup
+    }
+
+    return master_lookup[item_template.item_type][item_template.strength]
+
 
 def generate_monster(monster_template):
     "Generates a monster from a monster_template"
-    pass
+    quote = QUOTES[monster_template.category].pop()
+    drop = generate_item(monster_template.category,
+                         monster_template.strength)
+
+    return monster_template.correct_constructor(monster_template.name,
+                                                monster_template.description,
+                                                drop, str(quote))
 
 
 class MonsterTemplate(object):
     "Template to generate a monster."
+    _strength_to_constructor = {
+        1: Monster.BrainMonster.shadow_knight,
+        2: Monster.BrainMonster.shadow_giant,
+        3: Monster.BrainMonster.shadow_gargantuan,
+        4: Monster.BrainMonster.shadow_guardian
+    }
+
     def __init__(self, name, description, category, strength):
-        self.name = name
-        self.description = description
-        self.category = category
-        self.strength = strength
+        self.name = name  # String
+        self.description = description  # String
+        self.category = category  # String
+        self.strength = strength  # Int from 1 -> 4
+
+    def correct_constructor(self):
+        "Returns the appropriate BrainMonster constructor to use."
+        return self._strength_to_constructor[self.strength]
 
 
 class ItemSet(object):
