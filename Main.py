fdfcdde147c391fb4e1c62d71f824f18a4faf424
Jonathan Reem <jonathan.reem@gmail.com>
2014-01-09 04:16:28 -0500

    Fixed bug in description.

--- Main.py
+++ Main.py
@@ -10,12 +10,13 @@ File that is actually run in order to play a game.
 from commonplace.Generate import generate_all
 from commonplace.Game import PoemGame
 
+
 def main():
     "Generates and runs the game."
     game_map, game_player = generate_all()
     name = "The Commonplace"
     with open('./commonplace/description.txt') as description_file:
-        description = description_file.readlines()
+        description = "\n".join(description_file.readlines())
     game = PoemGame(name, description, game_map, game_player)
     game.play_game()
 
