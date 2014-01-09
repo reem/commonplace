eb8d1e962ff49bd03faac9730b1576ec6fc9d270
Jonathan Reem <jonathan.reem@gmail.com>
2014-01-09 02:39:20 -0500

    Added poems

---
+++ commonplace/Poems.py
@@ -0,0 +1,186 @@
+"""
+Jonathan Reem
+January 2014
+
+Contains and exports POEMS.
+"""
+
+
+POEMS = [
+"""|sides of an n=3 hypercube| ways of looking at a..
+
+square
+Not an equilateral rectangle,
+perfectly symmetrical
+Not the second simplest shape,
+made with lines
+Not the shadow of a tesseract,
+projected to a plane
+No.
+
+It's a square.
+Not a regular quadrilateral
+It's a square
+Not an n=2 hypercube
+It's a square
+Not the most efficient shape
+No.
+
+It is JUST a square.
+boring by loathsome
+with area I hate math
+
+Ya. I wonder why.
+
+Mocking: Thirteen Ways of Looking at a Blackbird by Wallace Stevens""",
+
+"""the sky bleeds Coca-Cola
+Knives pierced the sky
+drenched in sky
+drenched in blood
+
+coke adds life
+america's real choice
+red white and you
+always coca cola
+
+Knives blotted out the sun
+Knives slashes the blanket of stars
+
+the people's ambition
+fails to extend beyond
+their pockets - their little squares of light
+dollars and apples and coke
+
+their dreams replaced
+by the blood of the sky
+
+designed by apple in california""",
+
+
+"""keyboards go clack
+clickity clickity clack
+It's the most annoying
+and most powerful
+sound in the world
+click click clack
+
+The sound of generation
+of pure
+and infinite
+creation
+it is it, it is!
+it means meaning
+click click click
+
+tap in to the power
+power of billions -
+billions and billions -
+every second
+
+processors are silent
+
+fans go whir
+
+click clack whir
+It gets loud when you push it
+like a child
+whines and yells
+WHIRRRRRRRRRRRR
+
+it'll do what you want
+stupidly, sometimes.
+don't forget
+to check
+am I on fire?
+click WHIR click clack
+
+and it complains
+and complains
+and complains
+but with the clack
+of a keyboard
+you can do anything.
+
+WHIRRRRRRRRRRRRRR
+
+What's the collatz chain of 15,000 factorial?
+WHIRRRRRRR
+No problem.
+
+What's the billionth fibonacci number?
+WHIRRRRRRRR
+No problem.
+
+Burn through 'em.
+That's the power
+of click click clack
+WHIRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR""",
+
+"""My eyes feel like they're going to push themselves into my skull
+It's late
+and it was late last night
+and the night before
+and the night before
+
+There is an approximately gold light
+and an approximately brown couch
+and an approximately tree table
+with my approximately 100 hours on it
+and many of them still left undone
+
+out the window its black
+and it sounds black
+quiet
+
+except for little beeps and bloops
+and a special little black box
+where white letters jump into reality
+following perfectly clicky clacks
+
+the bed tempts
+but the box is strong
+so I wait
+and it gets late""",
+
+"""plunge
+the glass shatters beneath your feet
+and warps around you, a rush of fire and air
+as it blankets you
+
+in an instant it
+smothers you
+blots out the world
+no space
+except everywhere
+no color
+except blue
+no sound
+except heart
+no anything
+except yourself
+
+you smell nothing
+you touch nothing
+you see nothing
+you hear nothing
+you know yourself
+
+unadulterated
+
+then it breaks
+it roars and pulls you up
+the world feels so loud
+as it snaps you out of the void
+
+the world smells water
+the world tastes chlorine
+the world feels cold
+the world sees you
+the world screams
+the world grasps take you
+away from you
+
+then its quiet again
+and life beats regular"""
+]
