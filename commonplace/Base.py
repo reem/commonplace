c6b496370e7c8d13fcf886dde2dce4f378d9f8bb
Jonathan Reem <jonathan.reem@gmail.com>
2014-01-09 04:39:43 -0500

    Fixed None's

--- commonplace/Base.py
+++ commonplace/Base.py
@@ -35,8 +35,9 @@ def format_objects(items, indent=1):
         return "\n".join(["    " * indent + "{0}. {1.name}".format(i + 1, item)
                           for i, item in enumerate(items)])
     elif isinstance(items, dict):
-        return "\n".join(["    " * indent + "{k}: {v}".format(k=k, v=v)
-                          for k, v in items])
+        return "\n".join(["    " * indent + "{k}: {v.name}".format(
+            k=k, v=items[k].name if items[k] is not None else 'None')
+                          for k in items])
 
 
 def spaced_print(string, pre=True, post=True):
